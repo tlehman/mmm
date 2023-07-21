b05b868b8a6ecf8f90483493c92bcad78984c5e8
Tobi Lehman <mail@tobilehman.com>
2023-07-20 18:31:57 -0700

    Add protocol and type

--- src/mmm/grp.clj
+++ src/mmm/grp.clj
@@ -21,9 +21,9 @@
 ;; that converts the map into an operation, which is a binary function taking pairs of
 ;; elements of a set back to the same set, so G x G -> G
 
-(def ℤ₃ #{:e :a :b})
+(def z3-set #{:e :a :b})
 
-(def ℤ₃-table {[:e :e] :e
+(def z3-table {[:e :e] :e
                [:e :a] :a
                [:a :e] :a
                [:e :b] :b
@@ -32,13 +32,48 @@
                [:b :a] :e
                [:a :a] :b
                [:b :b] :a})
-#'mmm.grp/ℤ₃-table
 
 (defn table-to-op [table]
   (fn [a b]
     (table [a b])))
 
-(def z3* (table-to-op ℤ₃-table))
+(def z3* (table-to-op z3-table))
 
-(z3* :e :a)
+(z3* :a :b)
+
+
+;; Learning protocols and applying them to this project
+(defprotocol AlgebraicStructure
+  "Alebgraic structures are pairs (A, "
+  (elems [a]) ;; Returns a nonempty set of elements
+  (id [a])    ;; An identity element (can be nil for semigroups)
+  (op [a]))   ;; Returns a binary operation (fn [x y])
+
+(deftype Group [g-set g-op g-id]
+  AlgebraicStructure
+  (elems [g] g-set)
+  (id [g] g-id)
+  (op [g] g-op))
+
+(def G
+  (Group. z3-set  ;; set of elements
+          z3* ;; op : ℤ₃xℤ₃→ℤ₃
+          :e) ;; identity = [0] (adding 0 is identity)
+  )
+
+
+(id G)
+:e
+
+(elems G)
+#{:e :b :a}
+
+
+(let [*g (op G)]
+  (*g :a :b)) ;; a is the inverse of b so
+:e            ;; a * b = e (identity)
+
+
+
+
 
